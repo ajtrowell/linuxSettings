238ce160aa3601628e99a56f6053176c5109d6db
AJTrowell <ajtrowell@gmail.com>
2020-08-08 14:26:31 -0500

    Added restoreSettings.sh
    Moves preset files from repository folder to home folder.
    
    Untested

---
+++ restoreSettings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp .vimrc ~/.vimrc 
+cp .bashrc ~/.bashrc 
+cp .tmux.conf ~/.tmux.conf 
+cp tmux.md.txt ~/tmux.md.txt 
+cp tmux-cheatsheet.txt ~/tmux-cheatsheet.txt 
+cp linux_setup.txt ~/linux_setup.txt 
+cp setupSteps.txt ~/setupSteps.txt 
+
+
+
+
